693fa8b24baa5f34471e8aa584e95ed0b2e740e7
filomenoPonce580 <110742301+filomenoPonce580@users.noreply.github.com>
2023-02-05 16:09:25 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+/*
+First attempt
+
 function getSumOfDigits(integer) {
   let sum = 0;
   let digits =  integer.toString().split('');
@@ -6,3 +9,7 @@ function getSumOfDigits(integer) {
   }
   return sum;
 }
+*/
+
+//Better, cleaner. turn the integer into an array and then convert to numbers, and reduce
+const getSumOfDigits = integer => integer.toString().split('').map(Number).reduce((a, b) => a + b)
